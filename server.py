4cb719c5621d1995a7493afb46252bd333d4f2c4
Garry Volkov <github@garrynet.co.uk>
2022-11-06 12:33:57 +0000

    Finally allows for adding entries to a database

--- server.py
+++ server.py
@@ -1,7 +1,38 @@
 from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "5b814a7a42fb9c2841b698b5581879c6e2981caa84c2c53635c0571478b394009402282135e26c6f928b66049117e5c44860ee4062f8566d3e83ba75942bad0b"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    email = db.Column(db.String(100), primary_key=True)
+    password = db.Column(db.String(100))
+    fname = db.Column(db.String(100))
+
+    def __init__(self, email, password, fname):
+        self.email = email
+        self.password = password
+        self.fname = fname
+
+class entries(db.Model):
+    belongsTo = db.Column(db.String(100))
+    urlSafeTitle = db.Column(db.String(100), primary_key=True)
+    date = db.Column(db.String(10))
+    title = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    body = db.Column(db.String(1000000))
+
+    def __init__(self, belongsTo, urlSafeTitle, date, title, password, body):
+        self.belongsTo = belongsTo
+        self.urlSafeTitle = urlSafeTitle
+        self.date = date
+        self.title = title
+        self.password = password
+        self.body = body
+
 
 class config:
     HOST = "localhost"
@@ -51,11 +82,14 @@ def logout():
 def new_entry():
     if "loggedIn" in session and "email" in session:
         if request.method == "POST":
+            belongsTo = session["email"]
             date = request.form["date"]
             title = request.form["title"]
             password = request.form["password"]
             body = request.form["body"]
-            print(date, title, password, body)
+            entry = entries(belongsTo, title.replace(" ", "-").lower(), date, title, password, body)
+            db.session.add(entry)
+            db.session.commit()
         return render_template("new_entry.html")
     else:
         return redirect("/404")
@@ -66,6 +100,9 @@ def fetch_entry(entryTitle):
         return f"<h1 style='color: white;'>You requested: {entryTitle}</h1>"
     else:
         return redirect("/404")
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(host=config.HOST, port=config.PORT, debug=config.DEBUG)
